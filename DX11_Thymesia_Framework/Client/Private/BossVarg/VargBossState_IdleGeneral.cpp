b54b3b8e548076f5bb0a0758b0e2da1954716429
jo_jo_1206 <jojo1206@protonmail.com>
2022-12-12 18:55:12 +0900

    Update VargBossState_IdleGeneral.cpp

--- DX11_Thymesia_Framework/Client/Private/BossVarg/VargBossState_IdleGeneral.cpp
+++ DX11_Thymesia_Framework/Client/Private/BossVarg/VargBossState_IdleGeneral.cpp
@@ -22,26 +22,18 @@ HRESULT CVargBossState_IdleGeneral::Initialize_Prototype()
 HRESULT CVargBossState_IdleGeneral::Initialize(void* pArg)
 {
 	__super::Initialize(pArg);
-
-
 	return S_OK;
 }
 
 void CVargBossState_IdleGeneral::Start()
 {
 	__super::Start();
-
-
 	m_iAnimIndex = m_pModelCom.lock()->Get_IndexFromAnimName("SK_C_Varg.ao|Varg_IdleGeneral");
-
-
-	
 }
 
 void CVargBossState_IdleGeneral::Tick(_float fTimeDelta)
 {
 	__super::Tick(fTimeDelta);
-
 	m_pModelCom.lock()->Play_Animation(fTimeDelta);
 }
 
@@ -49,10 +41,8 @@ void CVargBossState_IdleGeneral::Tick(_float fTimeDelta)
 void CVargBossState_IdleGeneral::LateTick(_float fTimeDelta)
 {
 	__super::LateTick(fTimeDelta);
-
-	Rotation_TargetToLookDir();
-
-	Check_AndChangeNextState();
+	CBossStateBase::Rotation_TargetToLookDir();
+	this->Check_AndChangeNextState();
 }
 
 
@@ -60,50 +50,41 @@ void CVargBossState_IdleGeneral::LateTick(_float fTimeDelta)
 void CVargBossState_IdleGeneral::OnStateStart(const _float& In_fAnimationBlendTime)
 {
 	__super::OnStateStart(In_fAnimationBlendTime);
-
 	m_pModelCom.lock()->Set_CurrentAnimation(m_iAnimIndex);
 
 #ifdef _DEBUG
 #ifdef _DEBUG_COUT_
 	cout << "VargState: RunStart -> OnStateStart" << endl;
-#endif
-#endif
+#endif // _DEBUG_COUT_
+#endif// _DEBUG
 
 	m_pModelCom.lock()->Set_AnimationSpeed(1.5f);
-
 }
 
 void CVargBossState_IdleGeneral::OnStateEnd()
 {
 	__super::OnStateEnd();
-
 	m_pModelCom.lock()->Set_AnimationSpeed(1.f);
-
 }
 
 
 
 void CVargBossState_IdleGeneral::Free()
 {
-
 }
 
 _bool CVargBossState_IdleGeneral::Check_AndChangeNextState()
 {
-
-	if (!Check_Requirement())
+	if (!CBossStateBase::Check_Requirement())
 		return false;
 
-	_float fPToMDistance = Get_DistanceWithPlayer(); // 플레이어와 몬스터 거리
-
+	_float fPToMDistance(CBossStateBase::Get_DistanceWithPlayer()); // 플레이어와 몬스터 거리
 
 	if (fPToMDistance <= 30.f)
 	{
-		Get_OwnerCharacter().lock()->Change_State<CVargBossState_Start>(0.05f);
+		CStateBase::Get_OwnerCharacter().lock()->Change_State<CVargBossState_Start>(0.05f);
 		return true;
 	}
-
-
 
 	return false;
 }
