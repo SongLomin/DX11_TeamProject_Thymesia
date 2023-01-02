416b8661f11f566aa1a80574e342d9573bd55043
jo_jo_1206 <jojo1206@protonmail.com>
2023-01-02 18:02:15 +0900

    Working on Corvus PW Hammer Effect

--- DX11_Thymesia_Framework/Client/Private/Corvus.cpp
+++ DX11_Thymesia_Framework/Client/Private/Corvus.cpp
@@ -425,6 +425,7 @@ void CCorvus::Ready_States()
 	ADD_STATE_MACRO(CCorvusState_PS_CaneSword);
 	ADD_STATE_MACRO(CCorvusState_PS_Knife);
 	ADD_STATE_MACRO(CCorvusState_PS_Scythe);
+	ADD_STATE_MACRO(CCorvusState_PS_Hammer);
 	ADD_STATE_MACRO(CCorvusState_PS_Magician);
 	ADD_STATE_MACRO(CCorvusState_PS_UrdSword);
 	ADD_STATE_MACRO(CCorvusState_PS_VargSword);
