b138e70f61a5e54d1acbb57a918b51846e311888
jo_jo_1206 <jojo1206@protonmail.com>
2023-01-10 06:53:12 +0900

    resetting urd effects

--- DX11_Thymesia_Framework/Client/Public/Client_Defines.h
+++ DX11_Thymesia_Framework/Client/Public/Client_Defines.h
@@ -36,7 +36,7 @@
 
 #pragma region Tool
 #define _MAP_TOOL_
-//#define _EFFECT_TOOL_
+#define _EFFECT_TOOL_
 #define _ANIMATION_TOOL_WEAPON_
 #pragma endregion // Tool
 
@@ -44,10 +44,10 @@
 //#define _DAGGER_TRAIL_
 //#define _PROP_EFFECT_
 #define _CORVUS_EFFECT_
-#define _VARG_EFFECT_
-#define	_BAT_EFFECT
+// #define _VARG_EFFECT_
+// #define	_BAT_EFFECT_
 #define _URD_EFFECT_
-#define _INTERACTION_EFFECT_
+// #define _INTERACTION_EFFECT_
 #pragma endregion // Effects
 
 #endif // _DEBUG
