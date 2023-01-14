3d0dca599b363b8f48cc1530018c21a785c022cf
SongLomin <lominsong@gmail.com>
2023-01-14 15:28:35 +0900

    Update Client_Defines.h

--- DX11_Thymesia_Framework/Client/Public/Client_Defines.h
+++ DX11_Thymesia_Framework/Client/Public/Client_Defines.h
@@ -44,7 +44,7 @@
 // #define _DAGGER_TRAIL_
 #define _CORVUS_EFFECT_
 #define _VARG_EFFECT_
-//#define _BAT_EFFECT_
+#define _BAT_EFFECT_
 #define _URD_EFFECT_
 #define _INTERACTION_EFFECT_
 #pragma endregion // Effects
