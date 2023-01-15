23350437321ae7282c6e5a192336caac84cc4aeb
jo_jo_1206 <jojo1206@protonmail.com>
2023-01-16 00:28:06 +0900

    230116 머지 완료

--- DX11_Thymesia_Framework/Engine/Private/Render_Manager.cpp
+++ DX11_Thymesia_Framework/Engine/Private/Render_Manager.cpp
@@ -1159,7 +1159,7 @@ HRESULT CRender_Manager::Render_Lights()
 
 	m_pShader->Set_RawValue("g_vCamPosition", &pPipeLine->Get_CamPosition(), sizeof(_float4));
 
-	m_pShader->Set_RawValue("g_bPBR", &m_pVIBuffer, sizeof(_bool));
+	m_pShader->Set_RawValue("g_bPBR", &m_bPBR, sizeof(_bool));
 
 	GET_SINGLE(CLight_Manager)->Render_Lights(m_pShader, m_pVIBuffer, pDeviceContext);
 
