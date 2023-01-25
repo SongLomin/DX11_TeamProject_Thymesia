cb448b43c6013ff38fbcb4522234eabd8354743a
jo_jo_1206 <jojo1206@protonmail.com>
2023-01-25 15:41:18 +0900

    박쥐 사운드 수정

--- DX11_Thymesia_Framework/Client/Public/Client_Defines.h
+++ DX11_Thymesia_Framework/Client/Public/Client_Defines.h
@@ -34,7 +34,7 @@
 #pragma endregion // Map
 
 #pragma region Tool
-// #define _EFFECT_TOOL_
+#define _EFFECT_TOOL_
 #define _ANIMATION_TOOL_WEAPON_
 #pragma endregion // Tool
   
@@ -76,7 +76,7 @@
 
 #pragma region Effects
 #undef _BAKE_EFFECTMESH_FBX_
-//#define _EFFECT_TOOL_
+#undef _EFFECT_TOOL_
 #undef _DAGGER_TRAIL_
 #define _CORVUS_EFFECT_
 #define _VARG_EFFECT_
@@ -112,11 +112,11 @@
 #pragma endregion // Map
 
 #pragma region Tool
- #define _CORVUS_MODEL_
+// #define _CORVUS_MODEL_
 #define _BOSS_MODEL_
-#define _ELITE_MOB_MODEL_
-#define _NORMAL_MOB_MODEL_
-#define _ANIMATION_TOOL_WEAPON_
+// #define _ELITE_MOB_MODEL_
+// #define _NORMAL_MOB_MODEL_
+// #define _ANIMATION_TOOL_WEAPON_
 #undef _BAKE_EFFECTMESH_FBX_
 #pragma endregion // Tool
 
