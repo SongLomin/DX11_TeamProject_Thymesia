86eb05b112c77c6bc6475ff030f7ec1f5d7e4f49
ans75780 <devmgh@naver.com>
2023-01-25 17:53:50 +0900

    Revert "바그 사운드 변경"
    This reverts commit 34e10edf5f8510bb41c5e136a652ca99b76fa5c6.

--- DX11_Thymesia_Framework/Client/Private/BossVarg/VargBossState_Roar_Getup.cpp
+++ DX11_Thymesia_Framework/Client/Private/BossVarg/VargBossState_Roar_Getup.cpp
@@ -151,7 +151,7 @@ void CVargBossState_SPA_Roar_Getup::Call_AnimationEnd(_uint iEndAnimIndex)
 
 	Get_OwnerCharacter().lock()->Change_State<CVargBossState_SPA_Run>(0.05f);
 
-	GAMEINSTANCE->PlayBGM("Varg_Boss_Phase2.ogg", GET_SINGLE(CUIManager)->Get_SoundType(UI_SOUND_TYPE::SOUND_BGM));
+	GAMEINSTANCE->PlaySound2D("Varg_Boss_Phase2.ogg", GET_SINGLE(CUIManager)->Get_SoundType(UI_SOUND_TYPE::SOUND_BGM));
 
 
 }
